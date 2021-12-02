a5fa691d42f8d014a73750b301c5ccea2eeb2940
汪振 <137014311@qq.com>
2021-12-02 14:54:42 +0800

    基于vite的vue2.6项目初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  data() {
+    return {
+      name: 1,
+    };
+  },
+  components: {HelloWorld},
+  created() { },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>
+      HelloWorld 
+    </h1>
+    <h2>
+      Vue2 
+    </h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+h1{
+  color: #4fc08d;
+  margin-top: 20%;
+  text-align: center;
+}
+h2{
+  text-align: center;
+}
+</style>
