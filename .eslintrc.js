1ed9c658e0fb89ef7aa48d279cc3cf7195b9d73c
汪振 <137014311@qq.com>
2021-12-03 17:26:29 +0800

    fix: 添加stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,68 @@ module.exports = {
   extends: ['plugin:vue/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'no-console': 'off',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        ignoredNodes: ['TemplateLiteral']
+      }
+    ],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0,
+    'generator-star-spacing': 'off',
+    'no-mixed-operators': 0,
+    'no-var': 2,
+    'prefer-template': 1,
+    'no-unused-vars': 1,
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: {
+          max: 10
+        },
+        multiline: {
+          max: 5,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/attribute-hyphenation': 0,
+    'vue/html-self-closing': 0,
+    'vue/component-name-in-template-casing': 0,
+    'vue/html-closing-bracket-spacing': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/no-unused-components': 1,
+    'vue/multiline-html-element-content-newline': 0,
+    'vue/no-use-v-if-with-v-for': 0,
+    'vue/html-closing-bracket-newline': 0,
+    'vue/no-parsing-error': 0,
+    'no-tabs': 0,
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    semi: [
+      2,
+      'never',
+      {
+        beforeStatementContinuationChars: 'never'
+      }
+    ],
+    'no-delete-var': 2,
+    'prefer-const': [
+      2,
+      {
+        ignoreReadBeforeAssign: false
+      }
+    ],
+    'template-curly-spacing': 'off'
+  }
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,15 +10,19 @@ export default {
   name: '',
   data() {
     return {}
+  },
+  created() {
+    const a = 1
   }
 }
 </script>
 <style scoped>
 h1 {
-  color: #4fc08d;
   margin-top: 20%;
+  color: #4fc08d;
   text-align: center;
 }
+
 h2 {
   text-align: center;
 }
