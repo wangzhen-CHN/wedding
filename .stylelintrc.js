620a3ce9f8f0b004c244f8e03190f0cc84f9e150
汪振 <137014311@qq.com>
2022-01-25 11:46:20 +0800

    feat: 配置stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,23 +1,43 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
-  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
+  extends: [
+    "stylelint-config-recommended-vue",
+    "stylelint-config-rational-order",
+    "stylelint-config-prettier",
+  ],
+  plugins: [
+    "stylelint-order",
+    "stylelint-declaration-block-no-ignored-properties",
+  ],
+  overrides: [
+    {
+      files: ["**/*.(less|css|html|vue)"],
+      customSyntax: "postcss-less",
+    },
+    {
+      files: ["**/*.(html|vue)"],
+      customSyntax: "postcss-html",
+    },
+  ],
   rules: {
-    'plugin/declaration-block-no-ignored-properties': true,
-    'comment-empty-line-before': null,
-    'declaration-empty-line-before': null,
-    'function-name-case': 'lower',
-    'no-descending-specificity': null,
-    'no-invalid-double-slash-comments': null,
-    'block-no-empty': null,
-    'value-keyword-case': null,
-    'rule-empty-line-before': ['always', { except: ['after-single-line-comment', 'first-nested'] }],
-    'at-rule-no-unknown': null,
-    'selector-pseudo-class-no-unknown': [
+    "plugin/declaration-block-no-ignored-properties": true,
+    "comment-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "function-name-case": "lower",
+    "no-descending-specificity": null,
+    "no-invalid-double-slash-comments": null,
+    "block-no-empty": null,
+    "value-keyword-case": null,
+    "rule-empty-line-before": [
+      "always",
+      { except: ["after-single-line-comment", "first-nested"] },
+    ],
+    "at-rule-no-unknown": null,
+    "selector-pseudo-class-no-unknown": [
       true,
       {
-        ignorePseudoClasses: ['global']
-      }
-    ]
+        ignorePseudoClasses: ["global"],
+      },
+    ],
   },
-  ignoreFiles: ['node_modules/**/*', 'build/**/*', 'dist/**/*']
-}
+  ignoreFiles: ["node_modules/**/*", "build/**/*", "dist/**/*"],
+};
